fe0777e16442ca2d1831b60ce3b9e268e3bf7c78
Ethan Weilheimer <ethanweilheimer@Ethans-MacBook-Pro-2.local>
2024-09-23 13:25:07 -0500

    inital commit

--- src/agent.py
+++ src/agent.py
@@ -8,9 +8,6 @@ env = GPSD_ENV('./../images/test.png')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
-# Create action noise for exploration
-action_noise = NormalActionNoise(mean=np.zeros(6), sigma=0.1 * np.ones(6))
-
 # Create the model
 model = DQN("MultiInputPolicy", env, verbose=1, device=device, learning_rate = 0.01)
 model.learn(total_timesteps=10000)
